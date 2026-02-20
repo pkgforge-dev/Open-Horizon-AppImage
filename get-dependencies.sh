14cf5680fb4691f6e53552e7b28c3909e56082c4
Link4Electronics <link4electronics@gmail.com>
2026-02-20 03:12:35 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,8 +7,10 @@ ARCH=$(uname -m)
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
-    lua    \
-    openal
+    glfw           \
+    lua51          \
+    openal         \
+    zlib-ng-compat
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
@@ -20,5 +22,12 @@ get-debloated-pkgs --add-common --prefer-nano
 # If the application needs to be manually built that has to be done down here
 
 # if you also have to make nightly releases check for DEVEL_RELEASE = 1
+echo "Making nightly build of Open Horizon..."
+echo "---------------------------------------------------------------"
+REPO="https://github.com/undefined-darkness/open-horizon"
+VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
+git clone "$REPO" ./open-horizon
+echo "$VERSION" > ~/version
 
+cd ./open-horizon
 sed -i '21i #include <limits>' renderer/fhm_location.cpp
